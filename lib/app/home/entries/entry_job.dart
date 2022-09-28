7d4c1861d2a10e23f06ecd58f8f27ddc4abb8457
Anuragstackapp <113967138+Anuragstackapp@users.noreply.github.com>
2022-09-28 14:27:23 +0530

    Initial commit

---
+++ lib/app/home/entries/entry_job.dart
@@ -0,0 +1,9 @@
+import 'package:video_project/app/home/models/entry.dart';
+import 'package:video_project/app/home/models/job.dart';
+
+class EntryJob {
+  EntryJob(this.entry, this.job);
+
+  final Entry entry;
+  final Job job;
+}
